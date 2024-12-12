0deb3c0122959480e366278a7b634d57df2da37d
Ruslan-Skif <ruslanajnaguzov@gmail.com>
2024-12-12 21:18:29 +0800

    DZ9_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        String firstName = ("Ivan");
+        String middleName = ("Ivanovich");
+        String lastName = ("Ivanov");
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println(" ");
+        System.out.println("Task 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println(" ");
+        System.out.println("Task 3");
+        String fullName1 = ("Иванов Семён Семёнович");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1.replace("ё","е"));
+
+
     }
 }
